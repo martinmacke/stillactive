c3a54cf884f20272fa27a26f1ce62cdd937f39aa
martinmacke <martin@aboutblank.cz>
2017-07-09 22:40:18 +0200

    Tawk.to chatbox integration in multiple languages.

--- footer.php
+++ footer.php
@@ -148,7 +148,28 @@ jQuery('.woocommerce-LoopProduct-link > img').removeAttr('title');
     jQuery(window).on('load',function(){
 		jQuery('#myModals').modal('show');
     });
-</script> 
+</script>
+
+<!--Tawk.to chatbox integration -->
+<?php
+switch(ICL_LANGUAGE_CODE){
+	case "sv": $tawkto_src = "https://embed.tawk.to/58c266e95b8fe5150eef1099/1bk3s3bj4"; break;
+	default: $tawkto_src = "https://embed.tawk.to/58c266e95b8fe5150eef1099/default";
+}
+?>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='<?php echo $tawkto_src; ?>';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 
 </body>
 </html>
