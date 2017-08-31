0fcd219f45d3cd8c4cd79e15d2349828496014d3
martinmacke <martin@aboutblank.cz>
2017-08-31 12:01:03 +0200

    Hotjar tracking code added

--- header.php
+++ header.php
@@ -10,6 +10,18 @@
 
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 
+<!-- Hotjar Tracking Code for www.stillactive.se -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:610313,hjsv:5};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
 <?php wp_head(); ?>
 </head>
 
